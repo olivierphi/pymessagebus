a4ecef7e88da8d0c8c56cb4306be6270d56208d5
Olivier Philippon <olivier@rougemine.com>
2018-09-26 12:16:37 +0100

    [internals] more idiomatic trigger of message handlers

--- pymessagebus/_messagebus.py
+++ pymessagebus/_messagebus.py
@@ -19,10 +19,8 @@ class MessageBus(api.MessageBus):
     def handle(self, message: object) -> t.List[t.Any]:
         if not self.has_handler_for(message.__class__):
             return []
-        handlers = self._handlers[message.__class__]
-        results = []
-        for handler in handlers:
-            results.append(self._trigger_handler(message, handler))
+        handlers: t.List[t.Callable] = self._handlers[message.__class__]
+        results = [self._trigger_handler(message, handler) for handler in handlers]
         return results
 
     def has_handler_for(self, message_class: type) -> bool:
