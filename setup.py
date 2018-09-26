50fe7d22774bcf452924fe7602f1fa77c9fd30a9
Olivier Philippon <olivier@rougemine.com>
2018-09-26 18:18:14 +0100

    [packaging] add PyPI setup

---
+++ setup.py
@@ -0,0 +1,40 @@
+
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the README file
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="pymessagebus",
+    version="1.0.0",
+    description="A simple implementation of the MessageBus / CommandBus pattern",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DrBenton/pymessagebus",
+    author="Olivier Philippon",
+    author_email="olivier@rougemine.com",
+    license="MIT",
+    # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.6",
+    ],
+    keywords="CommandBus MessageBus CommandHandler",
+    packages=["pymessagebus"],
+    package_dir={"pymessagebus": "pymessagebus"},
+    install_requires=[],
+    python_requires=">=3.6",
+    extras_require={"dev": [], "test": ["pytest", "pylint"]},
+    setup_requires=["pytest-runner", "pytest-pylint", "pytest-mypy", "pytest-black"],
+    tests_require=["pytest", "pylint", "mypy", "black"],
+)
