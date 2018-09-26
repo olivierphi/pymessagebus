b14ed555c64fb79e95d44138bfd30d647d139fd4
Olivier Philippon <olivier@rougemine.com>
2018-09-26 20:12:36 +0100

    [packaging] fine-tune the PyPI setup

--- setup.py
+++ setup.py
@@ -1,18 +1,10 @@
-
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-
-# To use a consistent encoding
-from codecs import open
-from os import path
-
-here = path.abspath(path.dirname(__file__))
+import setuptools
 
 # Get the long description from the README file
-with open(path.join(here, "README.md"), encoding="utf-8") as f:
+with open("README.md", "r", encoding="utf-8") as f:
     long_description = f.read()
 
-setup(
+setuptools.setup(
     name="pymessagebus",
     version="1.0.0",
     description="A simple implementation of the MessageBus / CommandBus pattern",
